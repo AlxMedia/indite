14ad80b3548d0fde245cabdb2a8fba71e738a5d4
Alexander Agnarson <alexander.agnarson@gmail.com>
2020-08-17 21:11:55 +0200

    1.0.7

--- functions.php
+++ functions.php
@@ -90,6 +90,19 @@ if ( ! function_exists( 'indite_setup' ) ) {
 add_action( 'after_setup_theme', 'indite_setup' );
 
 
+/*  Custom navigation
+/* ------------------------------------ */
+require_once 'functions/nav.php';
+$nav = new \AlxMedia\Nav();
+$nav->enqueue(
+	[
+		'script' => 'js/nav.js',
+		'inline' => false,
+	]
+);
+$nav->init();
+
+
 /*  Custom logo
 /* ------------------------------------ */
 if ( ! function_exists( 'indite_custom_logo' ) ) {
